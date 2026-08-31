0a57b473721c6f08c75a04355e0c84a3ae8452df
Robert Golebiewski <rag.web@outlook.com>
2026-08-31 10:30:53 +0200

    chore(android): upgrade Android Gradle plugin to 9.3.2

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -10,8 +10,8 @@ buildscript {
 }
 
 plugins {
-    id("com.android.application") version "9.2.1" apply false
-    id("com.android.library") version "9.2.1" apply false
+    id("com.android.application") version "9.3.2" apply false
+    id("com.android.library") version "9.3.2" apply false
     id("org.jetbrains.kotlin.android") version "2.4.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.4.0" apply false
 }
